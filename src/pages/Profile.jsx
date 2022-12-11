edf27b1e0902a0ffe8b7a6d4ee4b98a317895062
Bruno Morgades <brunomorgades@hotmail.com>
2022-12-10 21:03:25 -0300

    Add my listings section to the profile page and create the listingItem component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,12 +3,24 @@ import { getAuth, updateProfile } from "firebase/auth";
 import { useNavigate, Link } from "react-router-dom";
 import { toast } from "react-toastify";
 import { db } from "../firebase";
-import { doc, updateDoc } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDocs,
+  query,
+  orderBy,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import { FcHome } from "react-icons/fc";
+import { useEffect } from "react";
+import ListingItem from "../components/ListingItem";
 export default function Profile() {
   const auth = getAuth();
   const navigate = useNavigate();
   const [changeDetail, setChangeDetail] = React.useState(false);
+  const [listings, setListings] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
   const [formData, setFormData] = React.useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -40,6 +52,27 @@ export default function Profile() {
       toast.error("Could not update profile details");
     }
   };
+  useEffect(() => {
+    const fetchUserListings = async () => {
+      const listingRef = collection(db, "listings");
+      const q = query(
+        listingRef,
+        where("userRef", "==", auth.currentUser.uid),
+        orderBy("timestamp", "desc")
+      );
+      const querySnap = await getDocs(q);
+      let listings = [];
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListings(listings);
+      setLoading(false);
+    };
+    fetchUserListings();
+  }, [auth.currentUser.uid]);
   return (
     <div className="max-w-6xl mx-auto flex justify-center items-center flex-col">
       <h1 className="text-3xl text center mt-6 font-bold">My Profile</h1>
@@ -92,6 +125,22 @@ export default function Profile() {
             &nbsp; Sell or rent your home
           </button>
         </Link>
+      </div>
+      <div className="max-w-6xl px-3 mt-6 mx-auto">
+        {!loading && listings.length > 0 && (
+          <>
+            <h2 className="text-2xl text-center font-semibold">My Listings</h2>
+            <ul>
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  id={listing.id}
+                  listing={listing.data}
+                />
+              ))}
+            </ul>
+          </>
+        )}
       </div>
     </div>
   );
